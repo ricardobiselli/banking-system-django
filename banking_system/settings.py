1883e09cd44d56ca166cf45f11f11a04f3c88c78
ricardobiselli <ricardobiselli@gmail.com>
2024-01-14 22:40:18 -0300

    migrating DB from sqlite to postgresql

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'banking_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bank_db',
+        'USER': 'ricardo',
+        'PASSWORD': 'rr123123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
