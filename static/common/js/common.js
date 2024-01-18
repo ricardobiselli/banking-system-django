6f3d2ed17a76890320eff0efabc65ee8dc68599d
ricardobiselli <ricardobiselli@gmail.com>
2024-01-17 21:49:24 -0300

    new 'copy to clipboard' button feature

--- static/common/js/common.js
+++ static/common/js/common.js
@@ -1,8 +1,22 @@
 function redirectAfterDelay(url, delay) {
-    document.getElementById('loading-spinner').style.display = 'block';
-  
-    setTimeout(function () {
-      document.getElementById('loading-spinner').style.display = 'none';
-      window.location.href = url;
-    }, delay);
-  }
+  document.getElementById('loading-spinner').style.display = 'block';
+
+  setTimeout(function () {
+    document.getElementById('loading-spinner').style.display = 'none';
+    window.location.href = url;
+  }, delay);
+}
+
+function copyToClipboard(button) {
+  var container = button.closest(".individual-account-container");
+  var copyText = container.querySelector(".copyToClipboard");
+  var accountNumber = copyText.dataset.accountNumber;
+
+  var tempInput = document.createElement("input");
+  tempInput.value = accountNumber;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  tempInput.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+}
